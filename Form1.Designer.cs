113794f117c9e15646d107adba3dad805a575ee2
navnitss <43095390+navnitss@users.noreply.github.com>
2019-12-16 20:23:06 -0600

    Add files via upload
    Run the .exe file to start the application and use any text file to search terms.Dante.txt a 18000 line text file is provided if a complicated search needs to be performed to test the application

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace nxj190007Asg4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Path = new System.Windows.Forms.TextBox();
+            this.Keyword = new System.Windows.Forms.TextBox();
+            this.Browse = new System.Windows.Forms.Button();
+            this.Search = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Search Term";
+            // 
+            // Path
+            // 
+            this.Path.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Path.Location = new System.Drawing.Point(125, 62);
+            this.Path.Name = "Path";
+            this.Path.Size = new System.Drawing.Size(841, 22);
+            this.Path.TabIndex = 2;
+            // 
+            // Keyword
+            // 
+            this.Keyword.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Keyword.Location = new System.Drawing.Point(125, 136);
+            this.Keyword.Name = "Keyword";
+            this.Keyword.Size = new System.Drawing.Size(841, 22);
+            this.Keyword.TabIndex = 3;
+            // 
+            // Browse
+            // 
+            this.Browse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Browse.Location = new System.Drawing.Point(985, 47);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(126, 53);
+            this.Browse.TabIndex = 4;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // Search
+            // 
+            this.Search.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Search.AutoSize = true;
+            this.Search.Location = new System.Drawing.Point(985, 120);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(126, 55);
+            this.Search.TabIndex = 5;
+            this.Search.Text = "Search";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenFileDialog1_FileOk);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BackgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorker1_RunWorkerCompleted);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(74, 181);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1006, 295);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(52, 502);
+            this.progressBar1.Maximum = 19000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1059, 44);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1143, 568);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.Browse);
+            this.Controls.Add(this.Keyword);
+            this.Controls.Add(this.Path);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Path;
+        private System.Windows.Forms.TextBox Keyword;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace nxj190007Asg4
+{
+    public partial class Form1 : Form
+    {
+        string path;
+        int flc = 1;
+        String key;
+        int c = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //initialize list view
+            listView1.View = View.Details;
+            listView1.Columns.Add("Line No", 50);
+            listView1.Columns.Add("Line", 450);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+       
+
+        //Function to initiate the open file Dialog
+        private void openfile()
+        {
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+
+                 string ipath = openFileDialog1.FileName;
+                try
+                {
+                   
+                   
+                    path = ipath;
+                    Path.Text = path;
+
+
+                }
+                catch (IOException)
+                {
+                }
+
+
+            }
+        }
+       
+
+        private  void Browse_Click(object sender, EventArgs e)
+        {
+            //Initiates File Open
+            openfile(); 
+
+           
+        }
+
+        private void OpenFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+         //MultiThreading Implementation with Bacground worker to read file line by line
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            key = Keyword.Text;
+            string line;
+
+            if (!File.Exists(path))
+            {
+                using (FileStream strm = File.Create(path))
+                {
+
+                }
+
+            }
+
+
+            System.IO.StreamReader file =
+       new System.IO.StreamReader(@path);
+
+                
+           
+                while ((line = file.ReadLine()) != null)
+                {
+                string sendline = line;
+                if (backgroundWorker1.CancellationPending)
+                {
+                    //CANCEL
+                    e.Cancel = true;
+                }
+                
+
+                else if ( line.ToLower().Contains(key.ToLower() + " "))                
+                    {
+                    backgroundWorker1.ReportProgress(10, sendline);
+                    
+                    Thread.Sleep(100);
+                    }
+                flc++;
+
+                //lineCount = lineCount - 1;
+            }
+            
+
+
+
+
+            //file.Close();
+
+        }
+        //BackgroundWorker calls this class to report progress to UI Elements 
+        private void BackgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            string f = e.UserState as String;
+            
+            String[] row = { flc.ToString(), f };
+            ListViewItem item = new ListViewItem(row);
+            listView1.Items.Add(item);
+            progressBar1.Maximum = listView1.Items.Count;
+            progressBar1.Value = progressBar1.Value+1;
+
+
+        }
+
+        private void BackgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+        //Calls the backgroundworker to start search of file
+        private void Search_Click(object sender, EventArgs e)
+        {
+            c++;
+            Search.Text = "Cancel";
+            if (c == 1)
+                backgroundWorker1.RunWorkerAsync();
+            else
+                cancels();
+            
+        }
+
+        //Cancels the operations
+        private void cancels()
+        {
+            backgroundWorker1.CancelAsync();
+            
+        }
+
+       
+       
+     
+    }
+}
